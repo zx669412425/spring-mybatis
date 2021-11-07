8ecaacfd943687075918f079edc07ca991c10abc
zx669412425 <669412425@qq.com>
2021-11-07 21:34:36 +0800

    项目构建完成

---
+++ spring-mybatis-web/src/test/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @title: Main
+ * @Author zx
+ * @Date: 2021/11/7/0007 20:35
+ * @Version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
